077b5f4466e1d16bdd63926b1f7547002d2801ec
Joe Caruso <carusoj7@gmail.com>
2023-04-16 17:28:26 -0500

    render home page and create nav bar

---
+++ routes/flights.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
+
+const router = Router()
+
+// GET localhost:3000/flights
+router.get('/new', flightsCtrl.new)
+
+router.post('/', flightsCtrl.create)
+
+
+export { router }
