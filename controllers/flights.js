ef074190c0765f42924de30002d860e8aa7d197e
Joe Caruso <carusoj7@gmail.com>
2023-04-16 17:34:36 -0500

    create index ctrl

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,5 +1,18 @@
 import { Flight } from "../models/flight.js"
 
+function index(req, res) {
+  Flight.find({})
+  .then(flights => {
+    res.render('flights/index', {
+    flights: flights,
+  })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
 function newFlight(req, res) {
   res.render('flights/new', {
     title: 'Add Flight'
